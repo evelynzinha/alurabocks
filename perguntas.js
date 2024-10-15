09d362150ef608a89f39011a447ca01f70738015
evelynzinha <148580817+evelynzinha@users.noreply.github.com>
2024-10-15 16:17:56 -0300

    mnjbbmmmnmn

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    'programação'
+    'O Que é Phyton?' ,
+    'O Phyton é uma linguagem de programação'
+)
+
+criaCartao(
+    'geografia',
+    'qual a capital da frança?'
+    'a capital da frança é Paris'
+)
+
+criaCartao(
+    'programação' ,
+    'o que é uma função?' ,
+    'Uma função é um bloco de código'
+)
+
+criaCartao(
+    'Lingua inglesa' ,
+    'Como se diz oi em Inglês' ,
+    'oi em ingles é hi (RAI)'
+)
+
+
+
